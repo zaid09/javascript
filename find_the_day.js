91f472ea106660bbef1ad5f98ab19051b3b31091
zaid09 <analchoudhury1@gmail.com>
2016-02-11 19:28:23 +0530

    7 days of javascript

---
+++ find_the_day.js
@@ -0,0 +1,27 @@
+function findDay(myDate) {
+    // Return day for date myDate(MM/DD/YYYY)
+    // Note that myDate contains the date in string format
+   if(myDate != -1){
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+         var d = new Date(myDate);
+        console.log(days[d.getDay()]);
+    }
+    
+} 
+
+// tail starts here
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    var dates = _input.split('\n');
+
+    for (var i = 0; i < dates.length - 1; i++) {
+        findDay(dates[i]);
+    }
+});
+
